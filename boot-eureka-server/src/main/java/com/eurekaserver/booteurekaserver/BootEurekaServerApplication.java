c5ad7f80052f7e56e06cc30892b4d7582c61c5c2
maguoliang <496719266@qq.com>
2018-10-17 17:20:28 +0800

    first commit

---
+++ boot-eureka-server/src/main/java/com/eurekaserver/booteurekaserver/BootEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.eurekaserver.booteurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class BootEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootEurekaServerApplication.class, args);
+	}
+}
